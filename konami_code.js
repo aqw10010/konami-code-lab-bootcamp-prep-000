3f3d2a65170c54f8a99d9b3540bf2a470cf90734
Austin Williams <austinq.williams@gmail.com>
2019-03-21 00:47:41 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+let index=0;
 function init() {
-  // your code here
+  window.addEventListener("keydown", function(event){
+    const key=parseInt(event.which);
+    console.log(event.which);
+    if(key===code[index]){
+      index++;
+      if(index===code.length){
+        window.alert("Cheat activated, you cheater")
+      }
+    }
+    else{
+      index=0;
+    }
+  })
 }
+init();	
+
+//You'll want to attach an event listener to document.body and check for 'keydown' events. If the user enters this special code, pressing all ten of the keys in the correct order, alert() a congratulatory message. However, if they press a key out of sequence or a key that isn't part of the Konami code, don't alert() anything and simply keep listening for all ten keydowns in the correct order.
+
+//When you're testing the code out in the browser, be sure to call init() to attach the event listener and set everything up!
